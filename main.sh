d80610dce3f7cd07d5f60540ea2f387cf00ff408
tnaren97 <naren@wisc.edu>
2023-09-05 13:14:56 -0500

    modified filenames

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILES=$(ls file*)
+
+echo "This is a tutorial for working with GitHub. This script will print the outputs of every text file it sees in the current directory."
+
+for FILE in FILES; do
+  echo "The contents of $FILE are:"
+  echo ""
+  cat $FILE
+done
