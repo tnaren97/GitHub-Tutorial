b83d28668c1df8db78605d9f4151e7eb30a9f7a9
tnaren97 <naren@wisc.edu>
2023-09-05 13:25:16 -0500

    changed main.sh

--- main.sh
+++ main.sh
@@ -3,9 +3,12 @@
 FILES=$(ls *.txt)
 
 echo "This is a tutorial for working with GitHub. This script will print the outputs of every text file it sees in the current directory."
+echo "You are currently in main"
+echo ""
 
-for FILE in FILES; do
-  echo "The contents of $FILE are:"
+for FILE in $FILES; do
+  echo "~~~~~~~~~~~~~~~~~~~~~The contents of $FILE~~~~~~~~~~~~~~~~~~~~~~~~"
   echo ""
   cat $FILE
+  echo ""
 done
